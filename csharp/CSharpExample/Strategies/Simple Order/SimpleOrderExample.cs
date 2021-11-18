0b6f9e95472cc0ed9daf2135f6a087059a87fed3
Gabriel Benevides <gbenevides@americastg.com>
2021-11-18 13:11:49 -0300

    simple order example refactor

--- csharp/CSharpExample/Strategies/Simple Order/SimpleOrderExample.cs
+++ csharp/CSharpExample/Strategies/Simple Order/SimpleOrderExample.cs
@@ -25,18 +25,20 @@ namespace Strategies.SimpleOrder
                 await Task.Delay(1000);
 
                 string strategyId = await NewSimpleOrder();
-                await Task.Delay(1000);
+                await Task.Delay(2000);
 
-                string status = await GetSimpleOrderStatusById(strategyId);
+                var simpleOrder = await GetSimpleOrderById(strategyId);
+                var status = simpleOrder.Status;
+                await Task.Delay(2000);
 
                 await UpdateSimpleOrder(strategyId, status);
-                await Task.Delay(1000);
+                await Task.Delay(2000);
 
                 await CancelSimpleOrder(strategyId, status);
-                await Task.Delay(1000);
+                await Task.Delay(2000);
 
                 await GetAllSimpleOrders();
-                await Task.Delay(1000);
+                await Task.Delay(2000);
             }
             catch (Exception e)
             {
@@ -51,7 +53,6 @@ namespace Strategies.SimpleOrder
                     new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        // Irá pegar o token necessário para envio de ordens pela API
         static async Task InitAuthToken()
         {
             var response = await _httpClient.RequestPasswordTokenAsync(new PasswordTokenRequest
@@ -69,37 +70,30 @@ namespace Strategies.SimpleOrder
             var token = response.AccessToken.ToString();
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-            Console.WriteLine("Token de acesso obtido");
+            Console.WriteLine("Got Access Token");
             Console.WriteLine();
         }
 
-        // Método para pegar o status, através do strategyId da ordem
-        static async Task<string> GetSimpleOrderStatusById(string strategyId)
+        static async Task<SimpleOrderReturnedFields> GetSimpleOrderById(string strategyId)
         {
-            var response = await _httpClient.GetAsync("simple-order");
-
-            // Garante que a resposta não contém nenhum erro
+            Console.WriteLine("*** GETTING SIMPLE ORDER BY ID ***");
+            var response = await _httpClient.GetAsync($"simple-order/{strategyId}");
             response.EnsureSuccessStatusCode();
-            Console.WriteLine("Consulta de ordem simples: " + response.StatusCode);
 
             var listContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-            var deserializedResponse = JsonConvert.DeserializeObject<List<SimpleOrderReturnedFields>>(listContent);
-            var strategySent = deserializedResponse.Find(x => x.StrategyId == strategyId);
-            var status = strategySent.Status.ToString();
-            Console.WriteLine("Status da ordem: " + status);
+            var deserializedResponse = JsonConvert.DeserializeObject<SimpleOrderReturnedFields>(listContent);
+            Console.WriteLine("SIMPLE ORDER: " + JsonConvert.SerializeObject(deserializedResponse));
             Console.WriteLine();
 
-            return status;
+            return deserializedResponse;
         }
 
-        // Método para receber informações de todos os envios de ordem simples
         static async Task GetAllSimpleOrders()
         {
-            var response = await _httpClient.GetAsync("simple-order");
+            Console.WriteLine("*** GETTING ALL SIMPLE ORDERS ***");
 
-            // Garante que a resposta não contém nenhum erro
+            var response = await _httpClient.GetAsync("simple-order");
             response.EnsureSuccessStatusCode();
-            Console.WriteLine("Consulta de ordem simples: " + response.StatusCode);
 
             var listContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             var deserializedResponse = JsonConvert.DeserializeObject<List<SimpleOrderReturnedFields>>(listContent);
@@ -112,26 +106,24 @@ namespace Strategies.SimpleOrder
             Console.WriteLine();
         }
 
-        // Metodo para criar uma ordem simples
         static async Task<string> NewSimpleOrder()
         {
+            Console.WriteLine("*** CREATING NEW SIMPLE ORDER ***");
+
             var response = await _httpClient.PostAsJsonAsync("simple-order",
                 new SimpleOrderRequest
                 {
                     Broker = Config.Broker,
                     Account = Config.Account,
                     OrderType = OrderType.LIMIT,
-                    Symbol = "SULA4", // papel 'exótico' para garantir que a ordem não irá executar antes do update
+                    Symbol = "SULA4",
                     Side = Side.SELL,
                     Quantity = 300,
                     Price = 19.00,
                     DisplayQuantity = 200,
                     TimeInForce = TimeInForce.DAY
                 });
-
-            // Garante que a resposta não contém nenhum erro
             response.EnsureSuccessStatusCode();
-            Console.WriteLine("Inclusão de ordem simples: " + response.StatusCode);
 
             var contentResponse = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             var orderResponse = JsonConvert.DeserializeObject<TradingResponses>(contentResponse);
@@ -141,17 +133,15 @@ namespace Strategies.SimpleOrder
             return orderResponse.StrategyId;
         }
 
-        // Método usado pra fazer update de ordem simples
-        static async Task UpdateSimpleOrder(string strategyId, string status)
+        static async Task UpdateSimpleOrder(string strategyId, Status status)
         {
+            Console.WriteLine("*** UPDATING SIMPLE ORDER ***");
             if (SimpleOrderCanBeUpdated(status))
             {
                 var response = await _httpClient.PutAsJsonAsync($"simple-order/{strategyId}",
                 new SimpleOrderRequest { Quantity = 50000 });
 
-                // Garante que a resposta não contém nenhum erro
                 response.EnsureSuccessStatusCode();
-                Console.WriteLine("Update de ordem simples: " + response.StatusCode);
 
                 var contentResponse = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
                 var orderResponse = JsonConvert.DeserializeObject<TradingResponses>(contentResponse);
@@ -160,22 +150,18 @@ namespace Strategies.SimpleOrder
             }
             else
             {
-                Console.WriteLine("Não foi possível realizar um update da ordem simples," +
-                    " já que o status atual da ordem é: " + status);
+                Console.WriteLine("Simple order update cannot be done, since the current status is: " + status);
                 Console.WriteLine();
             }
         }
 
-        // Método usado para fazer cancelamento de ordem simples
-        static async Task CancelSimpleOrder(string strategyId, string status)
+        static async Task CancelSimpleOrder(string strategyId, Status status)
         {
+            Console.WriteLine("*** CANCELLING SIMPLE ORDER ***");
             if (SimpleOrderCanBeUpdated(status))
             {
                 var response = await _httpClient.DeleteAsync($"simple-order/{strategyId}");
-
-                // Garante que a resposta não contém nenhum erro
                 response.EnsureSuccessStatusCode();
-                Console.WriteLine("Cancelamento de ordem simples: " + response.StatusCode);
 
                 var contentResponse = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
                 var orderResponse = JsonConvert.DeserializeObject<TradingResponses>(contentResponse);
@@ -184,13 +170,11 @@ namespace Strategies.SimpleOrder
             }
             else
             {
-                Console.WriteLine("Não foi possível realizar um cancelamento da ordem simples," +
-                    " já que o status atual da ordem é: " + status);
+                Console.WriteLine("Simple order cancel cannot be done, since the current status is: " + status);
                 Console.WriteLine();
             }
         }
 
-        // Método usado para logar a resposta da API após qualquer request enviado
         static void LogStrategyResponse(TradingResponses orderResponse, string strategyId)
         {
             Console.WriteLine("Response messageID: " + orderResponse.MessageId);
@@ -200,23 +184,20 @@ namespace Strategies.SimpleOrder
             Console.WriteLine();
         }
 
-        // Método se há algum erro no retorno do envio da ordem
         static void CheckOrderResponseError(string error)
         {
             if (!string.IsNullOrEmpty(error))
             {
-                Console.WriteLine("Erro no retorno da API:");
+                Console.WriteLine("Response error:");
                 throw new Exception(error);
             }
         }
 
-        // Método que verifica se a estratégia pode ser modificada, através do status
-        static bool SimpleOrderCanBeUpdated(string status)
+        static bool SimpleOrderCanBeUpdated(Status status)
         {
-            Status statusEnum = (Status)Enum.Parse(typeof(Status), status);
-            if (statusEnum == Status.CANCELLED ||
-                statusEnum == Status.FINISHED ||
-                statusEnum == Status.TOTALLY_EXECUTED)
+            if (status == Status.CANCELLED ||
+                status == Status.FINISHED ||
+                status == Status.TOTALLY_EXECUTED)
                 return false;
 
             return true;
